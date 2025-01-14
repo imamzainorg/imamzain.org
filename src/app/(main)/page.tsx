4c3bc4d225fbdb69cfbac870876047e0412f4c2a
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-14 09:31:19 +0300

    removed unused code

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,28 +1,27 @@
-import TopImage from "@/components/header";
-import Gallery from "../(pages)/gallery/page";
-import Header from "./header";
-import Imamzain from "./imamzain";
-import InstitutionMessage from "./institution-message";
-import Live from "./live";
-import Posts from "./posts";
-import Publications from "./publications";
-import RightsMessage from "./rights-message";
-import Services from "./services";
-import Videos from "./videos";
+import TopImage from "@/components/header"
+import Gallery from "../(pages)/gallery/page"
+import Imamzain from "./imamzain"
+import InstitutionMessage from "./institution-message"
+import Live from "./live"
+import Posts from "./posts"
+import Publications from "./publications"
+import RightsMessage from "./rights-message"
+import Services from "./services"
+import Videos from "./videos"
 
 export default function page() {
-  return (
-    <>
-    <TopImage />
-    <InstitutionMessage />
-    <Posts />
-    <Imamzain />
-    <RightsMessage />
-    <Services />
-    <Publications />
-    <Gallery />
-    <Videos />
-    <Live />
-    </>
-  );
+	return (
+		<>
+			<TopImage />
+			<InstitutionMessage />
+			<Posts />
+			<Imamzain />
+			<RightsMessage />
+			<Services />
+			<Publications />
+			<Gallery />
+			<Videos />
+			<Live />
+		</>
+	)
 }
