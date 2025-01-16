e2868d7331e436edef12a923fe4a7b4228b8f2f6
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-16 08:50:01 +0300

    not important

--- src/app/(main)/top-image/index.tsx
+++ src/app/(main)/top-image/index.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
-  return (
-    <>
- 			<div className="w-full h-[80vh] xl:h-[90vh] -mb-24">
-				<div className="bg-[url('/images/hero.png')] bg-top bg-cover 2xl:bg-center h-full w-full"></div>
+	return (
+		<>
+			<div className="w-full h-[80vh] xl:h-[90vh] -mb-24">
+				<div className="bg-[url('/images/landing.png')] bg-top bg-cover 2xl:bg-center h-full w-full"></div>
 			</div>
-    </>
-  );
+		</>
+	)
 }
