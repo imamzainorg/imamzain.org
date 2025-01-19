864b53991b26f5cdd408832c4a4d4042a07beef8
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-19 13:02:37 +0300

    refactor: section component title from 'الخدمات' to 'معرض الصور' for (main)/gallery/index.tsx

--- src/app/(main)/gallery/index.tsx
+++ src/app/(main)/gallery/index.tsx
@@ -8,7 +8,7 @@ export default function GallerySection() {
 		<div className="relative">
 			<div className="absolute h-full w-full bg-dark-background -z-10" />
 			<div className="container py-10 sm:py-20 space-y-8">
-				<Section title="الخدمات" />
+				<Section title="معرض الصور" />
 
 				<SwiperCarousel images={galleryImages} />
 			</div>
