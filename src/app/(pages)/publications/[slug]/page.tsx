2446a91676980f3fa46cb2f5594e399c7a07fa25
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-19 12:37:16 +0300

    style: update layout and styling for publications/[slug]/page.tsx

--- src/app/(pages)/publications/[slug]/page.tsx
+++ src/app/(pages)/publications/[slug]/page.tsx
@@ -34,29 +34,29 @@ export default async function Page({
 				]}
 			/>
 			{/* الكتاب المقصود */}
-			<div className="bg-gray-200 bg-opacity-50 rounded-3xl flex flex-col p-2 !mt-20 sm:!mt-40">
-				<div className="flex flex-col sm:flex-row justify-center gap-4 items-center">
+			<div className="bg-white/20 border border-primary/30 shadow-xl shadow-primary/20 rounded-3xl flex flex-col p-2 !mt-20 sm:!mt-40">
+				<div className="flex flex-col sm:flex-row justify-center gap-4 sm:gap-8 lg:gap-20 items-center">
 					<Image
 						src={publication.image}
-						width={250}
+						width={350}
 						height={200}
 						alt={publication.slug}
-						className="w-40 sm:w-56 h-auto rounded-xl -translate-y-16 sm:-translate-y-28"
+						className="w-40 sm:w-60 h-auto rounded-xl -translate-y-16 sm:-translate-y-28"
 					/>
-					<div className="space-y-2 text-center sm:text-right -translate-y-10 sm:-translate-y-28">
-						<p className="text-lg sm:text-2xl font-semibold">
+					<div className="space-y-2 text-center sm:text-right -translate-y-10 sm:-translate-y-28 lg:-translate-y-24 w-1/2">
+						<p className="text-lg sm:text-2xl lg:text-4xl font-semibold">
 							{publication.title}
 						</p>
-						<p className="text-sm sm:text-lg text-gray-500">
+						<p className="text-sm sm:text-lg lg:text-2xl text-gray-500">
 							{publication.author}
 						</p>
-						<div className="bg-white"></div>
 					</div>
 				</div>
 
-				<div className="text-center -translate-y-10 !mt-8 sm:grid sm:grid-cols-2 space-y-4">
-					<p>شخصيات اخرى</p>
-					<span className="font-extralight text-slate-500">
+				<div className="w-5/6 lg:w-3/6 mx-auto text-center sm:text-right sm:text-xl lg:text-2xl -translate-y-10 !mt-8 sm:grid sm:grid-cols-10 sm:items-center space-y-4 sm:!-mt-8">
+					<p className="col-span-3">شخصيات اخرى</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
 						{publication.otherNames[0]
 							? publication.otherNames.map((name) => (
 									<div key={name}>{name}</div>
@@ -64,35 +64,40 @@ export default async function Page({
 							: "لا يوجد"}
 					</span>
 
-					<p>المطبعة</p>
-					<span className="font-extralight text-slate-500">
+					<p className="col-span-3">المطبعة</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
 						{publication.printHouse}
 					</span>
 
-					<p>تاريخ الطبع</p>
-					<span className="font-extralight text-slate-500">
-						{publication.printDate.toISOString().split("T")[0]}
+					<p className="col-span-3">تاريخ الطبع</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
+						{publication.printDate.toISOString().split("T")[0]}{" "}
 					</span>
 
-					<p>اللغة</p>
-					<span className="font-extralight text-slate-500">
+					<p className="col-span-3">اللغة</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
 						{publication.language}
 					</span>
 
-					<p>عدد الصفحات</p>
-					<span className="font-extralight text-slate-500">
+					<p className="col-span-3">عدد الصفحات</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
 						{publication.pages}
 					</span>
 
-					<p>عدد الاجزاء</p>
-					<span className="font-extralight text-slate-500">
+					<p className="col-span-3">عدد الاجزاء</p>
+					<span className="hidden sm:block col-span-1">:</span>
+					<span className="font-extralight text-slate-500 col-span-6">
 						{publication.parts}
 					</span>
 				</div>
 			</div>
 
 			{/* كتب ذات صلة */}
-			<h2 className="text-center font-semibold border-t border-b p-4">
+			<h2 className="text-center font-semibold border-t border-b p-4 sm:text-2xl xl:text-4xl">
 				كتب ذات صلة
 			</h2>
 			<div className="bg-secondary bg-opacity-10 rounded-xl grid grid-cols-1 lg:grid-cols-2 p-2 lg:px-8">
