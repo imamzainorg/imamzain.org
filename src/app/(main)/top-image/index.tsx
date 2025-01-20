a9055b671b4df9ddd59ef9225e619bd6019630a8
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-20 14:08:09 +0300

    fix: incorrect path of landing image

--- src/app/(main)/top-image/index.tsx
+++ src/app/(main)/top-image/index.tsx
@@ -1,8 +1,8 @@
 export default function TopImage() {
-  return (
-    <>
- 			<div className="w-full h-[80vh] xl:h-[90vh] ">
-				<div className="bg-[url('/images/hero.png')] bg-top bg-cover 2xl:bg-center h-full w-full"></div>
+	return (
+		<>
+			<div className="w-full h-[80vh] xl:h-[90vh] ">
+				<div className="bg-[url('/images/landing.png')] bg-top bg-cover 2xl:bg-center h-full w-full"></div>
 			</div>
 		</>
 	)
