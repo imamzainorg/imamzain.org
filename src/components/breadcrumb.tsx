e3bdd4977861beec02d1279a9264f8e66e8d39d9
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-01-26 12:35:49 +0300

    feat: finished media page

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -3,22 +3,24 @@ import Link from "next/link"
 
 export default function Breadcrumbs({
 	links,
-	className,
+	className = "text-gray-700",
+	dotColor = "bg-primary",
 }: {
 	links: { name: string; url: string }[]
 	className?: string
+	dotColor?: string
 }) {
 	return (
 		<div className="pt-20 sm:pt-32 lg:pt-40 xl:pt-40">
 			<div
 				className={cn(
-					"flex text-sm sm:text-lg lg:text-2xl text-gray-700 my-6",
+					"flex text-sm sm:text-lg lg:text-2xl my-6",
 					className,
 				)}
 			>
 				{links.map((link, index) => (
 					<div key={index} className="flex items-center">
-						<span className="w-1 h-1 md:h-2 md:w-2 mx-3 bg-primary rounded-full" />
+						<span className={cn("w-1 h-1 md:h-2 md:w-2 mx-3 rounded-full", dotColor)} />
 						<Link
 							href={link.url}
 							className={cn(
