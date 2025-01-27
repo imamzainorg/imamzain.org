a7694cb8c5cae44adcae389896651f7b588f8667
dhiaa-a <dhiaajhamed@gmail.com>
2025-01-27 11:46:57 +0300

    feat: finalized /news, /news/[slug]

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -14,17 +14,17 @@ export default function Breadcrumbs({
 		<div className="pt-20 sm:pt-32 lg:pt-38 xl:pt-40 mb:8 lg:mb-20">
 			<div
 				className={cn(
-					"flex text-sm sm:text-lg lg:text-2xl my-6",
+					"flex text-xs sm:text-sm lg:text-lg my-6",
 					className,
 				)}
 			>
 				{links.map((link, index) => (
 					<div key={index} className="flex items-center">
-						<span className={cn("w-1 h-1 md:h-2 md:w-2 mx-3 rounded-full", dotColor)} />
+						<span className={cn("w-1 h-1 md:w-1.5 md:h-1.5 lg:h-2 lg:w-2 mx-3 rounded-full", dotColor)} />
 						<Link
 							href={link.url}
 							className={cn(
-								"hover:text-primary duration-150",
+								"hover:text-primary duration-150 line-clamp-1",
 								index === links.length - 1 && "font-bold",
 							)}
 						>
