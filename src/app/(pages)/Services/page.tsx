167ed05aa5745772a14f1b3339eb39ac83a400a8
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-02-04 11:24:56 +0300

    fix services fnish

--- src/app/(pages)/Services/page.tsx
+++ src/app/(pages)/Services/page.tsx
@@ -1,24 +1,22 @@
 "use client";
 import { toast } from "sonner";
 import { Autocomplete, AutocompleteItem, Button, Input } from "@heroui/react";
-import { Globe } from "lucide-react";
-import { PersonIcon, MobileIcon } from "@/assets/icons/reusable";
-
+import { Globe, MailOpen, TextIcon } from "lucide-react";
+import MessageIcon, { PersonIcon, MobileIcon } from "@/assets/icons/reusable";
 import Breadcrumbs from "@/components/breadcrumb";
 import Section from "@/components/section";
-import Image from "next/image";
 
 export default function ContactUs() {
   return (
     <div className="container ">
-                <Breadcrumbs
-                            links={[
-                                { name: "الصفحة الرئيسية", url: "/" },
-                                { name: "المكتبة", url: "#" },
-                                { name: "ما كتب عن الامام", url: "#" },
-                            ]}
-                        />
-        <Section title="موقع المؤسسة" />
+      <Breadcrumbs
+        links={[
+          { name: "الصفحة الرئيسية", url: "/" },
+          { name: "المكتبة", url: "#" },
+          { name: "ما كتب عن الامام", url: "#" },
+        ]}
+      />
+      <Section title="موقع المؤسسة" />
       <div className="flex items-center justify-center ">
         <div className=" bg-primary bg-pattern p-4 rounded-3xl w-full flex flex-col  items-center justify-center">
           <Input
@@ -43,38 +41,34 @@ export default function ContactUs() {
             className="border-none    w-11/12"
             labelPlacement="inside"
             name="phone"
-            placeholder="رقم الهاتف"
+            placeholder="الايميل"
             classNames={{
               base: "",
               input: "border-none focus:ring-0",
             }}
             startContent={
-              <MobileIcon stroke="#bb9661" fill="none" strokeWidth={1.5} />
+              <MailOpen stroke="#bb9661" fill="none" strokeWidth={1.5} />
             }
             type="text"
           />
-          <Input
-            size={"lg"}
-            className="border-none  m-4 w-11/12"
-            labelPlacement="inside"
-            name="phone"
-            placeholder="رقم الهاتف"
-            classNames={{
-              base: "",
-              input: "border-none focus:ring-0",
-            }}
-            startContent={
-              <MobileIcon stroke="#bb9661" fill="none" strokeWidth={1.5} />
-            }
-            type="text"
-          />
-          <textarea
-            className=" w-11/12 p-12 rounded-2xl  "
-            placeholder="نص الرسائل"
-          />
+          {/* Dropdown for Countries */}
+          <CountriesDropdown />
+          <div className="relative  w-11/12">
+            <textarea
+              className="border w-full h-24 md:h-32   pr-10 py-3  rounded-xl focus:ring-2 focus:ring-secondary"
+              name="phone"
+              placeholder="الرسائل"
+            ></textarea>
+            <div className="absolute left top-6 mr-3 transform -translate-y-1/2 ">
+              <MessageIcon width={24} height={24} />
+            </div>
+          </div>
 
           {/* Submit Button */}
-          <Button className="text-white rounded-md  bg-secondary p-6 pr-10 pl-10 m-10 ">
+          <Button
+            onClick={() => toast("تم ادراج اسمك في قائمة الزائرين")}
+            className="text-white rounded-md bg-secondary p-4 md:p-6 mt-2 font-bold text-xs md:text-lg"
+          >
             التسجيل
           </Button>
         </div>
@@ -82,7 +76,7 @@ export default function ContactUs() {
       <div className="w-full  pt-40  ">
         <div className=" pb-6  rounded-2xl bg-opacity-25 bg-slate-400   ">
           <div className="w-11/12 p-2 ">
-            <Section title="موقع المؤسسة"/>
+            <Section title="موقع المؤسسة" />
             <iframe
               className="rounded-[30px] w-full h-1/3 md:h-60 xl:h-96 shadow-xl p-1 m-4"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.679731674454!2d44.3607952!3d31.9966964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x155ed74306dd573d%3A0x16b7bd7757d9a76!2z2YXYpNiz2LPYqSDYp9mE2KfZhdin2YUg2LLZitmGINin2YTYudin2KjYr9mK2YYgKNi5KSDZhNmE2KjYrdmI2Ksg2YjYp9mE2K_Ysdin2LPYp9iq!5e0!3m2!1sen!2siq!4v1735032144406!5m2!1sen!2siq"
@@ -101,5 +95,28 @@ export default function ContactUs() {
         </div>
       </div>
     </div>
+  );
+}
+function CountriesDropdown() {
+  const countries = [
+    { label: "العراق", key: "iraq" },
+    { label: "ايران", key: "iran" },
+    { label: "السعودية", key: "ksa" },
+    { label: "البحرين", key: "bahrain" },
+    { label: "الكويت", key: "kuwait" },
+    { label: "تركيا", key: "turkey" },
+  ];
+
+  return (
+    <Autocomplete
+      startContent={<Globe stroke="#bb9661" fill="none" strokeWidth={2} />}
+      className="border-none    w-11/12 m-4"
+      placeholder="البلد"
+      size={"lg"}
+    >
+      {countries.map((country) => (
+        <AutocompleteItem key={country.key}>{country.label}</AutocompleteItem>
+      ))}
+    </Autocomplete>
   );
 }
