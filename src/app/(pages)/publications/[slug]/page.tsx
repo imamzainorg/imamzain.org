0fdf6d11eac8b65108d5a5649ddb55fc9cd5da19
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-02-09 10:18:21 +0300

    fix finsh publications / al-faraid-al-tarifa slug

--- src/app/(pages)/publications/[slug]/page.tsx
+++ src/app/(pages)/publications/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { publications } from "@/lib/data";
 import { Book } from "@/lib/definitions";
 import Image from "next/image";
 import BookCard from "@/components/book-card";
-import { MailOpen } from "lucide-react";
+import { MailOpen, Share2, Share2Icon } from "lucide-react";
 export default async function Page({
   params,
 }: {
@@ -34,44 +34,43 @@ export default async function Page({
         ]}
       />
       {/* الكتاب المقصود */}
-      <div className="pt-20 sm:pt-28 ">
-        <div className="bg-white/20 border    flex  border-primary/30 shadow-xl shadow-primary/20 rounded-3xl flex-col   lg:flex-row  w-full">
-          <div className="flex flex-col relative  sm:flex-row -top-24 sm:top-0     justify-center items-center lg:gap-6 lg:items-start">
+      <div className="pt-20 sm:pt-28  ">
+        <div className="border bg-gray-100  flex  border-primary/30 shadow-xl shadow-primary/20 rounded-3xl flex-col     w-full">
+          <div className="flex flex-col relative  sm:flex-row -top-24 sm:top-0     justify-center items-center ">
             <Image
               src={publication.image}
               width={350}
               height={200}
               alt={publication.slug}
-              className="w-32 sm:w-40 h-auto   rounded-xl lg:-translate-y-16 sm:-translate-y-28"
+              className="w-32 sm:w-36 md:w-40 md:left-8 lg:w-44 xl:w-56 xl:left-32 xl:-top-10 md:relative lg:left-24  h-auto    rounded-xl  sm:-translate-y-28"
             />
-            <div className="flex flex-col ">
-              <div className=" space-y-2 mt-16 sm:mt-10 text-center sm:text-right -translate-y-10 sm:-translate-y-28 lg:-translate-y-24 ">
-                <p className="text-lg sm:text-2xl  lg:text-2xl font-semibold">
+            <div className="flex flex-col sm:p-4 lg:p-0 ">
+              <div className=" space-y-2 mt-16 sm:-mt-3 xl:mb-24  lg:pb-10 text-center sm:text-right -translate-y-10 sm:-translate-y-28  ">
+                <p className="text-lg sm:text-xl  xl:text-3xl font-semibold">
                   {publication.title}
                 </p>
-                <p className="text-sm sm:text-lg text-center lg:text-xl mr-4 text-gray-500">
+                <p className="text-sm sm:text-lg xl:text-xl  text-center  mr-4 text-gray-500">
                   {publication.author}
                 </p>
               </div>
-              <div className="  flex flex-row justify-between items-center w-full sm:relative sm:-top-20  p-1 lg:p-5">
-                <button className="bg-primary text-[10px]  p-3  rounded-2xl text-white  ">
-                  000ابدأ القراءة
-                </button>
-                <MailOpen
-                  stroke="#bb9661"
+              <div className="  flex flex-row justify-between  items-center w-full sm:relative sm:-top-20 xl:-top-36  p-1 ">
+                
+                <button className="bg-primary text-[10px] whitespace-pre p-4 sm:text-[14px] md:text-lg xl:text-xl   rounded-[30px] text-white  ">
+                إبدأ بالقراءة    ⟵ </button>
+                <Share2Icon
+                  stroke="#006654"
                   fill="none"
                   strokeWidth={1.5}
-                  className=" relative  w-1/2  "
+                  className=" relative right-12 sm:right-14 w-1/2 md:right-20 "
                 />
               </div>
-              <hr className="" />
+              <hr className="sm:relative sm:-top-16 xl:-top-32 " />
             </div>
           </div>
-    <div className="flex flex-col sm:flex-row bg-red-700">
-            <div className="w-full relative -top-10 lg:w-1/2 mx-auto text-center  lg:pr-48  sm:text-xl lg:text-xl  sm:grid sm:grid-cols-10  ">
-              <p className="col-span-3 ">شخصيات اخرى</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+          <div className="flex flex-col sm:flex-row  ">
+            <div className="w-full relative -top-10  sm:text-sm md:text-lg  mx-auto text-center xl:text-xl     sm:text-start sm:pr-24 ">
+              <p className=" ">شخصيات اخرى</p>
+              <span className="font-extralight text-slate-500 sm:m-5  ">
                 {publication.otherNames[0]
                   ? publication.otherNames.map((name) => (
                       <div key={name}>{name}</div>
@@ -79,40 +78,40 @@ export default async function Page({
                   : "لا يوجد"}
               </span>
 
-              <p className="col-span-3 ">المطبعة</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+              <p className=" mt-4">المطبعة</p>
+
+              <span className="font-extralight text-slate-500 sm:m-5  ">
                 {publication.printHouse}
               </span>
 
-              <p className="col-span-3">تاريخ الطبع</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+              <p className="mt-4">تاريخ الطبع</p>
+
+              <span className="font-extralight text-slate-500 sm:m-5">
                 {publication.printDate.toISOString().split("T")[0]}{" "}
               </span>
 
-              <p className="col-span-3">اللغة</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+              <p className="mt-4">اللغة</p>
+
+              <span className="font-extralight text-slate-500 sm:m-5">
                 {publication.language}
               </span>
 
-              <p className="col-span-3">عدد الصفحات</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+              <p className="mt-4">عدد الصفحات</p>
+
+              <span className="font-extralight text-slate-500 sm:m-5">
                 {publication.pages}
               </span>
 
-              <p className="col-span-3">عدد الاجزاء</p>
-              <span className="hidden sm:block col-span-1">:</span>
-              <span className="font-extralight text-slate-500 col-span-6">
+              <p className="mt-4">عدد الاجزاء</p>
+
+              <span className="font-extralight text-slate-500 sm:m-5">
                 {publication.parts}
               </span>
             </div>
-            <div className="lg:w-1/2 p-4 border-t-1 mx-auto w-full text-sm  lg:text-2xl pt-2 lg:pt-40">
+            <div className=" sm:w-3/4 p-4 sm:m-2 border-t-1 sm:relative sm:-top-8 sm:border-none mx-auto w-full text-sm lg:text-lg xl:text-xl pt-2 sm:pt-0 ">
               الوصف
               <br />
-              <span className="m-4 p-2 ">
+              <span className="relative right-4 text-slate-500 ">
                 الوصف الاجزاء يوجدب صلة الطبع اللغة الوصف
               </span>
             </div>
