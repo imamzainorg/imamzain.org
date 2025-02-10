4674add8bb8b53850ba57c7754b6db670d5fa3cd
dhiaa <dhiaajhamed@gmail.com>
2025-02-10 13:20:48 +0300

    edit

--- src/app/(main)/application/index.tsx
+++ src/app/(main)/application/index.tsx
@@ -31,7 +31,8 @@ export default function Application() {
 
         <div className={"container py-32"}>
             <div
-                className="bg-white rounded-2xl flex justify-between items-center p-10 py-14 h-[500px]
+                className="bg-white rounded-2xl flex justify-between items-center p-10 py-14 h-[500px] 
+
                       shadow-[0px_0px_2.7px_-10px_rgba(0,0,0,0.034),0px_0px_6.9px_-10px_rgba(0,0,0,0.049),0px_0px_14.2px_-10px_rgba(0,0,0,0.061),0px_0px_29.2px_-10px_rgba(0,0,0,0.076),0px_0px_80px_-10px_rgba(0,0,0,0.11)]
                       "
             >
