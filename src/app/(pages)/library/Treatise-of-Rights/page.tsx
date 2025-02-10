ab191678ab3a92dbc0646f3522b2ab9ab4ab43ab
dhiaa-a <dhiaajhamed@gmail.com>
2025-02-10 12:32:12 +0300

    Add conference prep visit picture

--- src/app/(pages)/library/Treatise-of-Rights/page.tsx
+++ src/app/(pages)/library/Treatise-of-Rights/page.tsx
@@ -9,46 +9,44 @@ import SectionCta from "@/components/section-cta"
 
 export default function Page() {
 	return (
-		<div className="">
-			<div className="m-10">
-				<Breadcrumbs
-					links={[
-						{ name: "الصفحة الرئيسية", url: "/" },
-						{ name: " المكتبة التخصصية", url: "/library" },
-						{ name: "رسالة الحقوق", url: "#" },
-					]}
+		<div className="m-10">
+			<Breadcrumbs
+				links={[
+					{ name: "الصفحة الرئيسية", url: "/" },
+					{ name: " المكتبة التخصصية", url: "/library" },
+					{ name: "رسالة الحقوق", url: "#" },
+				]}
+			/>
+			<SectionCta
+				links={[
+					{ label: "الصحيفة كاملة", href: "#" },
+					{ label: "معجم الألفاظ", href: "#" },
+					{ label: "ما ألحق بها", href: "#" },
+				]}
+			/>
+			<ShowcaseSection
+				route="/library/treatise-of-rights"
+				showcaseBooks={libraryBooks.slice(0, 3)}
+			/>
+			<div className="bg-secondary bg-opacity-10 rounded-xl grid grid-cols-1 lg:grid-cols-2 p-2 gap-x-8 lg:p-10">
+				{libraryBooks.map((book) => (
+					<BooklibraryCard key={book.id} publication={book} />
+				))}
+			</div>
+			<div className="lg:hidden ">
+				<RelatedBooks
+					route="/library/treatise-of-rights"
+					relatedBooks={libraryBooks.slice(1, 3)}
 				/>
+			</div>
+			<div className="my-4">
 				<SectionCta
 					links={[
 						{ label: "الصحيفة كاملة", href: "#" },
-						{ label: "معجم الألفاظ", href: "#" },
+						{ label: "معجم الفاظ الصحيفة", href: "#" },
 						{ label: "ما ألحق بها", href: "#" },
 					]}
 				/>
-				<ShowcaseSection
-					route="/library/treatise-of-rights"
-					showcaseBooks={libraryBooks.slice(0, 3)}
-				/>
-				<div className="bg-secondary bg-opacity-10 rounded-xl grid grid-cols-1 lg:grid-cols-2 p-2 gap-x-8 lg:p-10">
-					{libraryBooks.map((book) => (
-						<BooklibraryCard key={book.id} publication={book} />
-					))}
-				</div>
-				<div className="lg:hidden ">
-					<RelatedBooks
-						route="/library/treatise-of-rights"
-						relatedBooks={libraryBooks.slice(1, 3)}
-					/>
-				</div>
-				<div className="my-4">
-					<SectionCta
-						links={[
-							{ label: "الصحيفة كاملة", href: "#" },
-							{ label: "معجم الفاظ الصحيفة", href: "#" },
-							{ label: "ما ألحق بها", href: "#" },
-						]}
-					/>
-				</div>
 			</div>
 		</div>
 	)
