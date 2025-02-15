bf7c6f203ce8447eaa12810b3a76a6d08661d8ff
dhiaa-a <dhiaajhamed@gmail.com>
2025-02-15 12:04:50 +0300

    test: checking if a copy of treatise of rights would render on domain

---
+++ src/app/(pages)/library/risalat-al-huqoq/page.tsx
@@ -0,0 +1,57 @@
+import BooklibraryCard from "../components/book-library-card"
+import Breadcrumbs from "@/components/breadcrumb"
+import RelatedBooks from "../components/related-books"
+import ShowcaseSection from "../components/showcase-section"
+import SectionCta from "@/components/section-cta"
+import { dataFetcher } from "@/lib/dataFetcher"
+import { Book } from "@/types/book"
+
+export default async function Page() {
+	const libraryBooks = await dataFetcher<Book[]>("library.json")
+	return (
+		<div className="m-10">
+			<Breadcrumbs
+				links={[
+					{ name: "الصفحة الرئيسية", url: "/" },
+					{ name: " المكتبة التخصصية", url: "/library" },
+					{ name: "رسالة الحقوق", url: "#" },
+				]}
+			/>
+			<SectionCta
+				links={[
+					{ label: "الصحيفة كاملة", href: "#" },
+					{ label: "معجم الألفاظ", href: "#" },
+					{ label: "ما ألحق بها", href: "#" },
+				]}
+			/>
+			<ShowcaseSection
+				route="/library/treatise-of-rights"
+				showcaseBooks={libraryBooks.slice(0, 3)}
+			/>
+			<div className="bg-secondary bg-opacity-10 rounded-xl grid grid-cols-1 lg:grid-cols-2 p-2 gap-x-8 lg:p-10">
+				{libraryBooks.map((book) => (
+					<BooklibraryCard
+						route="/library/treatise-of-rights"
+						key={book.id}
+						publication={book}
+					/>
+				))}
+			</div>
+			<div className="lg:hidden ">
+				<RelatedBooks
+					route="/library/treatise-of-rights"
+					relatedBooks={libraryBooks.slice(1, 3)}
+				/>
+			</div>
+			<div className="my-4">
+				<SectionCta
+					links={[
+						{ label: "الصحيفة كاملة", href: "#" },
+						{ label: "معجم الفاظ الصحيفة", href: "#" },
+						{ label: "ما ألحق بها", href: "#" },
+					]}
+				/>
+			</div>
+		</div>
+	)
+}
