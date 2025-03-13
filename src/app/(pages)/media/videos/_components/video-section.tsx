c84fd5fd8619b9b8f8a63a8b289a7d6e8dccbdfa
Taqy-Alyasry <taqyalyasry97@gmail.com>
2025-03-13 07:50:55 +0300

    media2

--- src/app/(pages)/media/videos/_components/video-section.tsx
+++ src/app/(pages)/media/videos/_components/video-section.tsx
@@ -1,46 +1,55 @@
+"use client"
+
 import { PlayButtonIcon, TimeIcon } from "@/assets/icons/reusable";
 import ImageView from "@/components/image-view";
 import { Eye } from "lucide-react";
-import { useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import { Button } from "@heroui/react";
+import useWindowEvents from "@/hooks/window-events";
 
 export default function VideoComponent() {
-  // Auto-play the video (muted) on page load
-  const [isPlaying] = useState(true);
+  const [isPlaying , setIsPlaying] = useState(false);
   const containerRef = useRef(null);
-
+const {isSmallScreen} = useWindowEvents()
   const handleOpenLink = () => { 
     window.open(
       "https://www.youtube.com/embed/c2In_ZUNTsI?autoplay=1",
       "_blank"
     );
   };
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setIsPlaying(true); // Start the timer
+    }, 3000); // 3000ms = 3 seconds
+    return () => clearTimeout(timerId);
+  }, []);
 
   return (
     <div
       ref={containerRef}
-      className="relative rounded-2xl h-screen overflow-hidden bg-black"
+      className="relative  h-[70vh]   lg:h-screen overflow-hidden bg-black flex flex-col justify-center items-center"
     >
       {/* Video iframe auto-playing muted */}
-      <iframe
-        className={`w-full h-[114vh] transition-opacity duration-300 absolute ${
-          isPlaying ? "opacity-100" : "opacity-0"
-        }`}
-        src="https://www.youtube.com/embed/c2In_ZUNTsI?autoplay=1&mute=1&controls=0&amp;start=11"
-        title="YouTube Video"
-        allow="autoplay; fullscreen"
-      ></iframe>
+      {isPlaying &&
+          <iframe
+              className={`w-[100rem] h-[90vh]  lg:w-[140rem] lg:h-[130vh]  transition-opacity duration-300 absolute   ${
+                  isPlaying ? "opacity-100" : "opacity-0"
+              }`}
+              src="https://www.youtube.com/embed/c2In_ZUNTsI?autoplay=1&mute=1&controls=0&amp;start=11"
+              title="YouTube Video"
+              allow="autoplay; fullscreen"
+          ></iframe>
+      }
 
-      {/* Image placeholder */}
       <div
-        className={`w-full h-full transition-opacity duration-300 absolute z-0 ${
-          isPlaying ? "opacity-0" : "opacity-100"
-        }`}
+          className={`w-full h-full transition-opacity duration-300 absolute  ${
+              isPlaying  ? "opacity-0" : "opacity-100"
+          }`}
       >
         <ImageView
-          src="/news/img.png"
-          alt="Video thumbnail"
-          className="object-cover w-full h-full"
+            src="/news/img.png"
+            alt="Video thumbnail"
+            className="object-cover w-full h-full"
         />
       </div>
 
@@ -71,11 +80,11 @@ export default function VideoComponent() {
       <div className="absolute group top-0 right-0 w-full font-semibold text-sm h-full z-10">
         <div className="container w-full h-full flex flex-col justify-end items-start cursor-pointer">
 
-          <div className="bg-neutral-4 w-[35rem] pb-12  flex flex-col justify-end items-start gap-4">
-            <div className="font-semibold pb-4 text-6xl text-[#bb9661]">
+          <div className="bg-neutral-4 w-[70%] lg:w-[35rem] pb-10 md:pb-28 lg:pb-32  flex flex-col justify-end items-start lg:gap-4">
+            <div className="  pb-4 text-xl lg:text-4xl font-bold text-[#bb9661]">
               الملتقى التشاوري
             </div>
-            <div className="font-semibold text-5xl text-white">
+            <div className="font-semibold text-lg lg:text-3xl text-white">
               كلمة السيد غسان الخرسان في الملتقى التشاوري الأول لخطباء المنبر الحسيني
             </div>
             <div className="flex gap-4 items-center">
