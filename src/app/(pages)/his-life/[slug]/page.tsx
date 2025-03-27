1406a51ab9fff0ee46e9fee8309c596f26d47115
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-03-27 13:37:18 +0300

    fix his life Rearrange content

--- src/app/(pages)/his-life/[slug]/page.tsx
+++ src/app/(pages)/his-life/[slug]/page.tsx
@@ -2,14 +2,20 @@ import Breadcrumbs from "@/components/breadcrumb"
 import { dataFetcher } from "@/lib/dataFetcher"
 import { imamzainLife } from "@/types/imamzainLife"
 
-export default async function page({
-	params,
-}: {
-	params: Promise<{ slug: string }>
-}) {
-	const slug = (await params).slug
+export default async function Page({ params }: { params: { slug: string } }) {
+	
 	const imamzainLife = await dataFetcher<imamzainLife[]>("imamzain.json")
-	const section = imamzainLife.filter((item) => item.slug === slug)[0]
+
+
+	const section = imamzainLife.find((item) => item.slug === params.slug)
+
+	if (!section) {
+		return (
+			<div >
+				...
+			</div>
+		)
+	}
 
 	return (
 		<div>
@@ -22,12 +28,15 @@ export default async function page({
 			/>
 			<div className="lg:w-11/12 mx-auto p-6 relative">
 				<div className="space-y-6 bg-yellow-50 rounded-3xl p-6 shadow-lg">
-					<h3 className="w-60 h-auto absolute top-0  p-4 bg-[url('/shapes/button-bg.svg')] bg-center bg-no-repeat text-center text-white font-semibold ">
+					<h3 className="w-60 h-auto absolute top-0 p-4 bg-[url('/shapes/button-bg.svg')] bg-center bg-no-repeat text-center text-white font-semibold">
 						{section.title}
 					</h3>
-					<p className="text-justify tracking-tighter w-11/12 mx-auto text-lg leading-loose">
-						{section.content}
-					</p>
+					
+					
+					<div
+						className="text-justify tracking-tight w-11/12 mx-auto text-lg leading-loose"
+						dangerouslySetInnerHTML={{ __html: section.content }}
+					/>
 				</div>
 			</div>
 		</div>
