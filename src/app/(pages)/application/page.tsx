658d760181c1f5a6d02eaab1a4769a71988b70da
Taqy-Alyasry <taqyalyasry97@gmail.com>
2025-04-08 14:01:25 +0300

    optmaize build

--- src/app/(pages)/application/page.tsx
+++ src/app/(pages)/application/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import Link from "next/link";
-import ImageView from "@/components/image-view";
+import Link from "next/link"; 
 import Image from "next/image";
 import mainApp from "../../../../public/application/main-bg.jpg";
 
