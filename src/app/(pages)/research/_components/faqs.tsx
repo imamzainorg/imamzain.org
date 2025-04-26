e4bc5cb2e31f3fae290029781f4cd18f03fc8f20
dhiaa-a <dhiaajhamed@gmail.com>
2025-04-26 10:20:24 +0300

    feat: research page changes

--- src/app/(pages)/research/_components/faqs.tsx
+++ src/app/(pages)/research/_components/faqs.tsx
@@ -1,65 +1,66 @@
-import { SearchIcon } from "lucide-react";
+import { SearchIcon } from "lucide-react"
 
 export default function Faqs() {
-  return (
-    <div className="py-10">
-      <div className="flex flex-col items-center gap-4 justify-between pb-5">
-        <h1 className="text-primary text-3xl font-bold">الأسئلة الشائعة </h1>
-        <div className="col-span-1 w-full md:col-span-3 md:w-72 relative lg:mb-4">
-          <input
-            type="text"
-            // value={searchTitle}
-            // onChange={(e) => setSearchTitle(e.target.value)}
-            className="w-full rounded-2xl md:text-sm lg:text-lg p-1 bg-transparent border border-primary focus:border-primary   outline-none   "
-            placeholder="البحث عن البحوث"
-          />
-          <div className="absolute text-primary left-0 top-0 pl-3 h-full flex justify-center items-center gap-4">
-            <div className="h-2/3 w-[1px] bg-slate-400" />
-            <SearchIcon size={20} strokeWidth={1.5} />
-          </div>
-        </div>
-      </div>
+	return (
+		<div className="py-10">
+			<div className="flex flex-col items-center gap-4 justify-between pb-5">
+				<h1 className="text-primary text-3xl font-bold">
+					الأسئلة الشائعة{" "}
+				</h1>
+				<div className="col-span-1 w-full md:col-span-3 md:w-72 relative lg:mb-4">
+					<input
+						type="text"
+						// value={searchTitle}
+						// onChange={(e) => setSearchTitle(e.target.value)}
+						className="w-full rounded-2xl md:text-sm lg:text-lg p-1 bg-transparent border border-primary focus:border-primary   outline-none   "
+						placeholder="البحث عن البحوث"
+					/>
+					<div className="absolute text-primary left-0 top-0 pl-3 h-full flex justify-center items-center gap-4">
+						<div className="h-2/3 w-[1px] bg-slate-400" />
+						<SearchIcon size={20} strokeWidth={1.5} />
+					</div>
+				</div>
+			</div>
 
-      <div className="grid grid-cols-2 gap-10  ">
-        {[
-          {
-            question: "هل تقبلون الأبحاث المكتوبة باللغة العامية؟",
-            answer:
-              "بالطبع! نحن نقدّر الإبداع، حتى لو كان البحث مكتوباً باللهجة الدارجة أو على شكل قصيدة نبطية!",
-          },
-          {
-            question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
-            answer:
-              "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
-          },
-          {
-            question: "هل هناك جوائز مالية لأفضل بحث؟",
-            answer:
-              'نعم! الجائزة الأولى هي لقب "زين العابدين الصغير"، بالإضافة إلى مفتاح كنز الإمام السجاد (ع) المفقود منذ قرون!',
-          },
-          {
-            question:
-              "هل يمكنني الكتابة عن علاقة الإمام السجاد (ع) بالمخلوقات الفضائية؟",
-            answer:
-              "نعم، إذا كان لديك دليل من الصحيفة السجادية يؤكد اتصاله بكائنات من مجرة أخرى، فنحن ننتظر بحثك بفارغ الصبر!",
-          },
-          {
-            question: "ماذا لو سرقت أفكار بحثي؟",
-            answer:
-              "لا تقلق! لدينا فريق من الحراس الروحيين للإمام السجاد (ع) سيتعقبون أي شخص يسرق أفكارك ويعيدونها لك بطريقة غامضة!",
-          },
-          {
-            question: "ماذا لو سرقت أفكار بحثي؟",
-            answer:
-              "لا تقلق! لدينا فريق من الحراس الروحيين للإمام السجاد (ع) سيتعقبون أي شخص يسرق أفكارك ويعيدونها لك بطريقة غامضة!",
-          },
-        ].map((item, index) => (
-          <div key={index} className="text-start flex flex-col items-start gap-2 border-r-5 border-primary pr-2 cursor-pointer">
-            <h1 className="text-2xl font-bold"> {item.question} </h1>
-            <p className="text-xl "> {item.answer} </p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			<div className="grid grid-cols-2 gap-10  ">
+				{[
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+					{
+						question: "ما هو الحد الأقصى لعدد الصفحات في البحث؟",
+						answer: "لا يوجد حد! نرحب بالأبحاث من نصف صفحة حتى ألف صفحة، المهم أن يكون المحتوى مكتوباً بخط جميل!",
+					},
+				].map((item, index) => (
+					<div
+						key={index}
+						className="text-start flex flex-col items-start gap-2 border-r-5 border-primary pr-2 cursor-pointer"
+					>
+						<h1 className="text-2xl font-bold">
+							{" "}
+							{item.question}{" "}
+						</h1>
+						<p className="text-xl "> {item.answer} </p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
--- src/app/(pages)/research/page.tsx
+++ src/app/(pages)/research/page.tsx
@@ -1,84 +1,120 @@
-"use client";
+"use client"
 
-import Breadcrumbs from "@/components/breadcrumb";
-import SliderHero from "./_components/slider-hero";
-import Link from "next/link";
-import { Award } from "lucide-react";
-import UploadedResearch from "./_components/uploaded-research";
-import Faqs from "./_components/faqs";
+import Breadcrumbs from "@/components/breadcrumb"
+import SliderHero from "./_components/slider-hero"
+import Link from "next/link"
+import { BadgeCheck, Crown, FileBadge } from "lucide-react"
+import UploadedResearch from "./_components/uploaded-research"
+import Faqs from "./_components/faqs"
 
 export default function Page() {
-  return (
-    <div className="container  ">
-      <Breadcrumbs
-        links={[
-          { name: "الصفحة الرئيسية", url: "/" },
-          { name: "الصفحة العلمية", url: "/research" },
-        ]}
-      />
+	return (
+		<div className="container  ">
+			<Breadcrumbs
+				links={[
+					{ name: "الصفحة الرئيسية", url: "/" },
+					{ name: "الصفحة العلمية", url: "/research" },
+				]}
+			/>
 
-      {/* SliderHero  */}
-      <SliderHero />
+			{/* SliderHero  */}
+			<SliderHero />
 
-      {/* Link send research */}
-      <div className="flex justify-between items-center w-full  pb-14">
-        <hr className="border border-[#bb9661] h-  w-full" />
-        <Link
-          href={"/research/send-research"}
-          className="px-16 py-4 text-white text-nowrap mx-10 text-xl bg-primary rounded-2xl hover:bg-primary/95"
-        >
-          تقديم البحث
-        </Link>
-        <hr className="border border-[#bb9661] h-  w-full" />
-      </div>
+			{/* Link send research */}
+			<div className="flex justify-between items-center w-full  pb-14">
+				<hr className="border border-[#bb9661] w-full" />
+				<Link
+					href={"/research/send-research"}
+					className="px-16 py-4 text-white text-nowrap mx-10 text-xl bg-primary rounded-2xl hover:bg-primary/95"
+				>
+					تقديم البحث
+				</Link>
+				<hr className="border border-[#bb9661] h-  w-full" />
+			</div>
 
-      {/* Awards */}
-      <div className="py-14">
-        <h1 className="text-primary text-3xl font-bold text-center	">
-          المكافئات والجوائز
-        </h1>
-        <div className="flex justify-between gap-1 mt-12">
-          {[
-            {
-              title: " الكتب (تأليف أو تحقيق)",
-              subtitle: "لا يقل سعر الصفحة الواحدة عن 5000 دينار",
-            },
-            {
-              title: "  البحوث المقبولة",
-              subtitle: "مكافأة تمنح إذا كان البحث لا يقل عن 15 صفحة.",
-            },
-            {
-              title: " البحوث المتميزة",
-              subtitle:
-                "اصحاب البحوث المتميزة وذات الأصالة العلمية تمنح مكافأة 150,000 دينار",
-            },
-            {
-              title: " المقالات",
-              subtitle:
-                "مكافأة 25,000 دينار، مع إمكانية منح مكافأة إضافية في حال التميز",
-            },
-          ].map((item, index) => (
-            <div
-              key={index}
-              className="text-center flex flex-col items-center    w-3/12"
-            >
-              <Award
-                size={70}
-                strokeWidth={1}
-                className="mb-5"
-                color="#BA9560"
-              />
-              <h1 className="text-xl font-bold"> {item.title} </h1>
-              <p className="w-3/4"> {item.subtitle} </p>
-            </div>
-          ))}
-        </div>
-      </div>
-      {/* Uploaded research */}
-	  <UploadedResearch />
+			{/* Awards */}
+			<div className="py-14">
+				<h1 className="text-primary text-3xl font-bold text-center	">
+					المكافئات المالية
+				</h1>
+				<div className="flex justify-between gap-1 mt-12">
+					{[
+						{
+							title: "البحوث المقبولة",
+							amount: "100,000 د.ع.",
+							subtitle:
+								"كل بحث ينال المقبولية على ان لا يقل عن 15 صفحة",
+							icon: (
+								<BadgeCheck
+									size={70}
+									strokeWidth={1}
+									className="mb-5"
+									color="#BA9560"
+								/>
+							),
+						},
+						{
+							title: "البحوث المتميزة",
+							amount: "150,000 د.ع.",
+							subtitle: "كل كتاب يحصل على تميز",
+							icon: (
+								<Crown
+									size={70}
+									strokeWidth={1}
+									className="mb-5"
+									color="#BA9560"
+								/>
+							),
+						},
+						{
+							title: "المقالة",
+							amount: "25,000 د.ع.",
+							subtitle: "لكل مقالة متميزة",
+							icon: (
+								<FileBadge
+									size={70}
+									strokeWidth={1}
+									className="mb-5"
+									color="#BA9560"
+								/>
+							),
+						},
+					].map((item, index) => (
+						<div
+							key={index}
+							className="text-center flex flex-col items-center w-3/12"
+						>
+							{item.icon}
+							<h1 className="text-xl font-bold">{item.title}</h1>
+							<p className="w-3/4 font-semibold ">
+								{item.amount}
+							</p>
+							<p className="w-3/4"> {item.subtitle} </p>
+						</div>
+					))}
+				</div>
+				<div className="border-r-2 border-secondary pr-4 italic">
+					<div className="w-full text-right mt-8 text-lg">
+						ملاحظات:
+					</div>
+					<ol className="list-decimal text-right text-gray-700 space-y-2 sm:space-y-3  px-2 sm:px-4 md:px-6 lg:px-8">
+						{[
+							"سعر صفحة الكتاب (تأليف، تحقيق) 5,000 د.ع.",
+							"المكافئات اعلان تعني في البحوث والكتب التي تأتي من خلال الاستكتاب",
+							"البحوث المقدمة للمؤسسة تخصص 5% من مجموع المطبوع هدية للمؤلف",
+						].map((rule, index) => (
+							<li key={index} className="leading-relaxed">
+								{rule}
+							</li>
+						))}
+					</ol>
+				</div>
+			</div>
+			{/* Uploaded research */}
+			<UploadedResearch />
 
-	  {/* Faqs */}
-	  <Faqs />
-    </div>
-  );
+			{/* Faqs */}
+			<Faqs />
+		</div>
+	)
 }
