957afaee80ea3bf47bba7c886061fe54e8379072
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-06-22 10:33:35 +0300

    fix: update background handling in media template component

--- src/app/(pages)/media/template.tsx
+++ src/app/(pages)/media/template.tsx
@@ -1,28 +1,23 @@
 "use client"
 
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { useEffect } from "react"
+import { usePathname } from "next/navigation"
 
 export default function Template({ children }: { children: React.ReactNode }) {
-	const path = usePathname();
+  const path = usePathname()
 
-	useEffect(() => {
-		// Set the background color based on the path
-		if (path.startsWith("/media")) {
-			document.body.style.backgroundColor = "rgb(37,52,63)";
-		} else {
-			// Reset the background color if the path doesn't match
-			document.body.style.backgroundColor = ""; // or set to your default color
-		}
+  useEffect(() => {
+    if (path.startsWith("/media")) {
+    document.body.classList.add("media-background-creative")
 
-		return () => {
-			document.body.style.backgroundColor = "";
-		};
-	}, [path]);  
+    } else {
+      document.body.classList.remove("media-background-creative")
+    }
 
-	return (
-		<div className="pb-12 -mb-24">
-			{children}
-		</div>
-	);
+    return () => {
+      document.body.classList.remove("media-background-creative")
+    }
+  }, [path])
+
+  return <div className="pb-12 -mb-24 relative z-10">{children}</div>
 }
