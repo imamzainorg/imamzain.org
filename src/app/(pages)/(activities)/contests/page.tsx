dfab9716f73e92d864c49dfa6f7a87fcdfa12f1c
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-06-29 08:06:46 +0300

    fix: adjust text color for better visibility in contest page header

--- src/app/(pages)/(activities)/contests/page.tsx
+++ src/app/(pages)/(activities)/contests/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
 					<div className="absolute mx-auto h-px w-40 bg-gradient-to-r from-transparent via-primary to-transparent" />
 				</div>
 				<div className="px-4 py-10 md:py-20">
-					<h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300 !leading-relaxed">
+					<h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 dark:text-slate-700  md:text-4xl lg:text-7xl dark:text-slate-300 !leading-relaxed">
 						{"مسابقات في تراث الإمام زين العابدين (عليه السلام)"
 							.split(" ")
 							.map((word, index) => (
