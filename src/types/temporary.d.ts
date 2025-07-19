eec7621579459d8e75d5e50bdc75b71d39708bc4
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-07-19 12:41:38 +0300

    feat: update Research interface to enforce required fields and add publishedYear

--- src/types/temporary.d.ts
+++ src/types/temporary.d.ts
@@ -3,15 +3,15 @@ export interface Research  {
   id: string; // معرف فريد للبحث  
   slug: string;
   title: string; // عنوان البحث
-   abstract?: string; // الملخص (باللغة الأصلية)
-   part?: string; // الجزء أو المجلد (اختياري)
-   section?: string; // المحور أو القسم (اختياري)
-   topic?: string; // موضوع البحث أو المحور الفرعي
+   abstract: string; // الملخص (باللغة الأصلية)
+   part: string; // الجزء أو المجلد (اختياري)
+   section: string; // المحور أو القسم (اختياري)
+   topic: string; // موضوع البحث أو المحور الفرعي
   author?: string; // اسم الباحث أو الباحثين
-  pdfUrl?: string; // رابط تحميل البحث PDF (اختياري)
+  pdfUrl: string; // رابط تحميل البحث PDF (اختياري)
   pages?: number; // عدد الصفحات
-  publishedYear?: number; // سنة النشر (اختياري)
+  publishedYear?:string; // سنة النشر (اختياري)
 Description?: string; // وصف البحث (اختياري)
-pdfUrl?: string; // رابط تحميل البحث PDF (اختياري)
+ authorDescription?: string; // وصف الباحث (اختياري)
 }
 
