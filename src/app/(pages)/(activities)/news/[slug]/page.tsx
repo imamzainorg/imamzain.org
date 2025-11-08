40b6843402ebe20fe427962391f8015a6c7c782f
AliAlmsfer <20015556602@alkadhum-col.edu.iq>
2025-11-08 10:43:20 +0300

    Update layout for main content section to improve responsiveness

--- src/app/(pages)/(activities)/news/[slug]/page.tsx
+++ src/app/(pages)/(activities)/news/[slug]/page.tsx
@@ -40,7 +40,7 @@ export default async function page({
 			</div>
 
 			{/* Main Content */}
-			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+			<div className="2xl:w-[88rem] w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
 				<div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
 					{/* Post Content - Main Column */}
 					<article className="lg:col-span-8">
