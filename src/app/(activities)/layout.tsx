065690ce483f86f3ed5f45aff7397e6c664253c9
AliAlmsfer <alialialiooop9@gmail.com>
2025-12-24 11:55:20 +0300

    fix: remove container class from ActivitiesLayout for improved styling

--- src/app/(activities)/layout.tsx
+++ src/app/(activities)/layout.tsx
@@ -3,5 +3,5 @@ export default function ActivitiesLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	return <div className="container">{children}</div>
+	return <div className="">{children}</div>
 }
