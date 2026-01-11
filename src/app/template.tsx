2bac4738012938becdb028eb7518abd89b62983f
AliAlmsfer <alialialiooop9@gmail.com>
2026-01-11 13:04:10 +0300

    refactor: simplify Template component by removing loading state and animations

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,40 +1,7 @@
-"use client"
-import { motion, AnimatePresence } from "framer-motion"
-import { useState, useEffect } from "react"
+"use client";
 
-import Layouts from "@/layouts"
+import Layouts from "@/layouts";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-	const [loading, setLoading] = useState<boolean>(true)
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setLoading(false)
-		}, 150)
-
-		return () => clearTimeout(timer)
-	}, [])
-
-	return (
-		<AnimatePresence mode="wait">
-			{loading ? (
-			<>
-			
-			</>
-			) : (
-			      <motion.div
-        key="page"
-        initial={{ opacity: 1, y: 16 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -2 }}
-        transition={{
-          duration: 0.4,
-          ease: [0.4, 0, 0.2, 1.5],
-        }}
-      >
-        <Layouts>{children}</Layouts>
-      </motion.div>
-			)}
-		</AnimatePresence>
-	)
+  return <Layouts>{children}</Layouts>;
 }
