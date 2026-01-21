e98a0fb83078edc65fd648409075d7152b648ea6
AliAlmsfer <alialialiooop9@gmail.com>
2026-01-21 10:14:51 +0300

    refactor: clean up BookCard component by removing unused elements and improving code formatting for better readability

--- src/app/library/[collectionSlug]/[dictionarySlug]/page.tsx
+++ src/app/library/[collectionSlug]/[dictionarySlug]/page.tsx
@@ -18,12 +18,7 @@ export default async function Page({
           id={subject?.slug}
           className="group relative w-full h-full overflow-hidden bg-white bg-opacity-60 border cursor-pointer rounded-xl flex justify-between items-center p-3 border-primary/20 hover:border-primary/80 dark:hover:border-Muharram_secondary/60 shadow-md hover:shadow-xl duration-300"
         >
-          <div className="absolute -top-10 -right-10 z-0">
-            <div className="relative w-36 h-36">
-              <div className="absolute w-full h-full rounded-full dark:from-Muharram_secondary/30 dark:to-Muharram_primary/10 blur-2xl opacity-30 group-hover:opacity-50 group-hover:scale-105 transition-all duration-700" />
-              <div className="absolute w-20 h-20 right-4 top-4 rounded-full bg-primary/20 dark:bg-Muharram_secondary/20 blur-xl opacity-40 dark:opacity-40 group-hover:opacity-60 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all duration-700 dark:group-hover:opacity-60 dark:group-hover:translate-x-1 dark:group-hover:-translate-y-1 dark:transition-all dark:duration-700" />
-            </div>
-          </div>
+   
 
           <div className="w-5/6 flex flex-col gap-1 z-10">
             <h2 className=" text-note  leading-10 font-semibold pr-2 text-gray-800 group-hover:text-primary dark:group-hover:text-Muharram_primary duration-300">
