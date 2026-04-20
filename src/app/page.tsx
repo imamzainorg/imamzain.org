09989a8865bce6477d401ff65533b86afc9c49a9
AliAlmsfer <alialialiooop9@gmail.com>
2026-04-20 08:42:45 +0300

    hotfix: adjust formatting and indentation in desktopImages array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,20 +19,19 @@ const Videos = dynamic(() => import("./_components/videos"));
 const Application = dynamic(() => import("./_components/application"));
 
 const desktopImages = [
-   "/images/fhrs-alsahefe.jpg",
+  "/images/fhrs-alsahefe.jpg",
   "/images/ziara-imamzain-web.jpg",
- 
-  
-  "/images/albaqi.jpg",
+    "/images/albaqi.jpg",
   "/images/web.jpg",
+
   //"/images/albaqi-2.png",
   //"/images/hero-3.jpg", // لمحات
 
   //"/images/hero-9.jpg", // المباني السياسية
-  //"/images/hero-4.jpg", // تراتيل القانتين
+  "/images/hero-4.jpg", // تراتيل القانتين
   "/images/hero-5.jpg", // البعد الاجتماعي
   "/images/hero-6.jpg", // ابي حمزة الثمالي
-  "/images/hero-7.jpg", // بناء الامن النفسي
+ // "/images/hero-7.jpg", // بناء الامن النفسي
 ];
 
 const mobileImages = [
