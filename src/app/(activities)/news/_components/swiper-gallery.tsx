1a93077aab8b2123f56e7b6a3079a71b82759bdc
AliAlmsfer <alialialiooop9@gmail.com>
2026-04-30 10:03:34 +0300

    refactor: remove unused Swiper CSS imports from swiper-gallery component

--- src/app/(activities)/news/_components/swiper-gallery.tsx
+++ src/app/(activities)/news/_components/swiper-gallery.tsx
@@ -8,11 +8,7 @@ import {
 	Pagination,
 } from "swiper/modules"
 
-import "swiper/css"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import "swiper/css/effect-coverflow"
-import "swiper/css/autoplay"
+
 import ImageView from "@/components/image-view"
 import { Attachment } from "@/types/attachments"
 
